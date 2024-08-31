ab84b8d2af2a0a7bcc9a1f1f2347851d85568909
BorisRojnov <BorisRojnov@yandex.by>
2024-08-31 12:47:39 +0300

    homework complete

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -3,7 +3,16 @@ public class Main {
     // А-123
     // Б-321...
     // Коды даны для примера и не соответствуют действительности. Учитывайте, что нужно распечатать коды букв в двух регистрах!
+    static final String RUSSIAN_ALPHABET_UPPER = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    static final String RUSSIAN_ALPHABET_LOWER = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
     public static void main(String[] args) {
         //todo код писать тут
+        for(int i = 0; i < RUSSIAN_ALPHABET_UPPER.length(); i++) {
+            System.out.print(RUSSIAN_ALPHABET_UPPER.charAt(i) + " - " +
+                    ((int)(RUSSIAN_ALPHABET_UPPER.charAt(i))) + "\n");
+
+            System.out.println(RUSSIAN_ALPHABET_LOWER.charAt(i) + " - " +
+                    ((int)(RUSSIAN_ALPHABET_LOWER.charAt(i))) + "\n");
+        }
     }
 }
